173c320d8ecc9977b9896a5465613fef8d39f04e
Balaji_200 <baladurai420@gmail.com>
2020-02-11 23:13:33 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,23 @@
+$(function(){
+    $('#home').hover(function(){
+        $(this).toggleClass('text-light');
+    });
+    $('#events').hover(function(){
+        $(this).toggleClass('text-light');
+    });
+    $('#iris').hover(function(){
+        $(this).toggleClass('text-light');
+    });
+    $('#voyager').hover(function(){
+        $(this).toggleClass('text-light');
+    });
+    $('#about').hover(function(){
+        $(this).toggleClass('text-light');
+    });
+    $('#team').hover(function(){
+        $(this).toggleClass('text-light');
+    });
+    $('#blog').hover(function(){
+        $(this).toggleClass('text-light');
+    });
+})
